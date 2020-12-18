d7df2cb78e57d692a3d1790ebf59d93d14cbe95d
prangamajumder <pranganmajumder@appscode.com>
2020-12-18 13:43:50 +0600

    added allArticles Endpoint

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w , "Homepage Endpoint Hit")
@@ -12,6 +29,7 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homepage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
