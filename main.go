8548c6c6dbe8f30294ce3558b93b6940329ba751
prangamajumder <pranganmajumder@appscode.com>
2020-12-21 12:44:22 +0600

    modified GET,POST,DELETE & createNewUser() admin need to provide ID from postman

--- main.go
+++ main.go
@@ -8,44 +8,118 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
-	Content string `json:"content"`
+type User struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Varsity string `json:"varsity"`
+	Occupation string `json:"occupation"`
+}
+var Users []User
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w , "Wellcome to homepage")
+	fmt.Println("Endpoint Hit: homepage")
 }
 
-type Articles []Article
+func returnAllUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit:  returnAllArticles")
+	json.NewEncoder(w).Encode(Users)
+}
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+func returnSingleUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	// Loop over all of our Users
+	// if the user.Id equals the key we pass in return the article encoded as JSON
+	var flag = false
+	for _, user := range Users{
+		if user.Id == key{
+			json.NewEncoder(w).Encode(user)
+			flag = true
+		}
 	}
-	fmt.Println("Endpoint Hit: All articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+	// if the user Id is not found in the list return a bad request
+	// https://stackoverflow.com/questions/40096750/how-to-set-http-status-code-on-http-responsewriter
+	if flag == false{
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte ("400 - User Not Found in the list"))
+	}
 }
 
-func homepage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w , "Homepage Endpoint Hit")
+func createNewUser(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// return the string response containing the request body
+	reqBody , _ := ioutil.ReadAll(r.Body)
+	fmt.Println("createdNewUser")
+	//fmt.Fprintf(w, "%v", string(reqBody))
+
+
+	// now unmarshal this into a new User struct
+	var user User
+	json.Unmarshal(reqBody, &user)
+
+
+
+	// check if ID is in the list or not
+	for _, u := range Users{
+		if u.Id == user.Id{
+			fmt.Println("User ID matched")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - User ID already contains in the list. Please Change your ID"))
+			return
+		}
+	}
+
+	// append this to our Users array
+	// update our global Users array to include our new User
+	Users = append(Users, user)
+	json.NewEncoder(w).Encode(user) // send the json encoded format
 }
 
-func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test POST endpoint worked")
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	// we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the user we wish to delete
+	id := vars["id"]
+
+	var flag = false
+	for index, user := range Users{
+		if user.Id == id{
+			Users = append(Users[:index] , Users[index+1:]...)
+			flag = true
+		}
+	}
+	// if user Id is not present in the list , return a error code
+	if flag == false{
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("400 - User not found in the list"))
+	}
+
 }
 
-func handleRequests() {
+func handleRequests()  {
 	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage).Methods("GET")
+	myRouter.HandleFunc("/users", returnAllUser).Methods("GET")
 
-	myRouter.HandleFunc("/", homepage)
-	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
+	// Ordering is important here! This has to be defined before the other '/user' endpoint
+	myRouter.HandleFunc("/user", createNewUser).Methods("POST")
+	myRouter.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
+	myRouter.HandleFunc("/user/{id}", returnSingleUser).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080",myRouter))
 }
-
 
 func main() {
+	Users = []User{
+		User{Id:         "1", Name:       "Prangan", Varsity:    "CoU", Occupation: "Student"},
+		User{Id:         "2", Name:       "Sakib", Varsity:    "NSU", Occupation: "software Engineer"},
+	}
 	handleRequests()
 }
